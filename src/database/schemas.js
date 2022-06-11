b40ae7d0d71d6dd73d2bca5b7d6ce84934083e61
Janshen Vernon <janshen.vbb@gmail.com>
2022-06-11 17:13:52 -0300

    adicionado schema para criação de usuários

---
+++ src/database/schemas.js
@@ -0,0 +1,11 @@
+const schemas = `
+
+create table usuarios(
+    id serial primary key,
+    nickname varchar(200) not null,
+    senha text not null,
+    nivel varchar(55) not null
+)
+`;
+
+module.exports = schemas;

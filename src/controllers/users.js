e65bf31e7471c784bb15b6db6aadf78a1f641b99
Janshen Vernon <janshen.vbb@gmail.com>
2022-06-12 18:43:56 -0300

    Adicionada rota de criação de usuário

---
+++ src/controllers/users.js
@@ -0,0 +1,43 @@
+const knex = require('../database/connection');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+async function UserSignUp(req, res) {
+    let {
+        nickname, senha, nivel
+    } = req.body;
+
+    const reqBodyLength = Object.keys(req.body).length;
+
+    if (reqBodyLength === 0 || !nickname || !senha || !nivel) return res.status(400).json({
+        message: 'Todos os campos são obrigatórios.'
+    });
+
+    try {
+        nickname = nickname.toLowerCase();
+
+        senha = await bcrypt.hash(String(senha), 10);
+
+        const newUserData = {
+            nickname,
+            senha,
+            nivel
+        };
+
+        await knex('usuarios')
+            .insert(newUserData);
+
+        return res.status(201).json({
+            message: 'Usuário criado com sucesso!'
+        });
+    } catch ({ message }) {
+        return res.status(500).json({
+            message
+        });
+    };
+};
+
+
+module.exports = {
+    UserSignUp
+}
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const routes = express();
 
+//Controllers
+const {
+    UserSignUp
+} = require('./controllers/users');
 
+
+
+routes.post('/usuarios', UserSignUp);
 routes.get('/welcome', (req, res) => {
     return res.status(200).json('Bem vindo(a)!');
 });

b8852f2f8845bda2668ca685726e92a5a2051bb5
Janshen Vernon <80076727+JanshenV@users.noreply.github.com>
2022-06-16 10:42:44 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@ const Migration = require('./seeders/migration');
 
 Migration();
 
+async function runMigration(){
+  try{
+    await Migration();
+  }catch(error){
+    console.log(error)
+  };
+};
+
+runMigration();
+
 app.listen(process.env.PORT || 37);
